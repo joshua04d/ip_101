4edee66bbbb32e4387955eb0c2818d9da8d9c3a6
joshua <joshua04dcosta@gmail.com>
2024-10-16 02:33:48 +0530

    Topic and desc page 1:31

--- app/create-course/_components/TopicDescription.jsx
+++ app/create-course/_components/TopicDescription.jsx
@@ -1,9 +1,21 @@
+import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
 import React from 'react'
 
 function TopicDescription() {
   return (
-    <div>
-      sdfsdfg
+    <div className='mx-20  lg:mx-44'>
+        {/* Topic */}
+            <div className='mt-5'>
+                <label> Write the topic for which u wanna generate the code </label>
+                <Input placeholder={'Topic'} />          
+            </div>
+            <div className='mt-5'>
+                <label>Tell us more abot your course, and hat u wnana include in your course(Optional)</label>
+                <Textarea placeholder="About your course"/>
+            </div>
+        {/* Text Area Desc  */}
+    
     </div>
   )
 }
